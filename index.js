3d4654fc4b45bdf9cd693e65756155662d9f2449
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-03 15:49:28 -0600

    DELETE is functional

--- index.js
+++ index.js
@@ -102,7 +102,14 @@ server.delete('/api/posts/:id', (req, res) => {
     const postsId = req.params.id;
 
     Posts.remove(postsId)
-        .then()
+        .then(id => {
+            if (id) {
+                res.status(200).json(id)
+            } else {
+                req.status(404).json({ message: "The post with the specified ID does not exist." })
+            };
+        })
+
         .catch(err => {
             res.status(500).json({ error: "The post information could not be modified." })
         })
