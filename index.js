011e51c1fc7042112072917497b1ec62400c3e69
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-03 15:26:31 -0600

    All the GET work and the first POST. Working on second Post request with id

--- index.js
+++ index.js
@@ -45,11 +45,15 @@ server.get('/api/posts/:id', (req, res) => {
 server.get('/api/posts/:id/comments', (req, res) => {
     const commentsId = req.params.id;
 
-    Posts.findCommentById()
+    Posts.findPostComments(commentsId)
+        .then(comment => {
+            if (comment) {
+                res.status(200).json(comment)
+            } else {
+                res.status(404)({ message: "The comment with the specified ID does not exist." })
+            }
+        })
 })
-
-
-///api/posts
 
 //post request to: /api/posts
 server.post('/api/posts', (req, res) => {
@@ -61,7 +65,7 @@ server.post('/api/posts', (req, res) => {
     } else {
         Posts.insert(req.body)
             .then(post => {
-                res.status(201).json(user)
+                res.status(201).json(post)
             })
             .catch(err => {
                 res.status(500).json({ error: "There was an error while saving the post to the database" })
@@ -70,12 +74,27 @@ server.post('/api/posts', (req, res) => {
 });
 
 //post request to: /api/posts/:id/comments
+//req.body.post_id;
 server.post('/api/posts/:id/comments', (req, res) => {
-    const commentsId = req.params.id;
-    if (!commentsId) {
-        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    const commentsId = req.body.post_id;
+    const { text } = req.body;
+
+    if (!text) {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
     } else {
-        Posts.insert()
+        Posts.insertComment(commentsId)
+            .then(id => {
+                if (id) {
+                    res.status(201).json(id)
+                } else {
+                    req.status(404).json({ message: "The post with the specified ID does not exist." })
+                };
+            })
+            .catch(() => {
+                res.status(500).json(
+                    { error: "There was an error while saving the comment to the database" }
+                )
+            });
     }
 });
 
