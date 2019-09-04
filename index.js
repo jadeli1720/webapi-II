10d1e2862ad961418c9486cb15d0a7a31ebd429b
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-09-04 16:11:23 -0600

    Fixed similar issue with endpoint on line 33 as I had on line 49

--- index.js
+++ index.js
@@ -30,19 +30,19 @@ server.get('/api/posts', (req, res) => {
 server.get('/api/posts/:id', (req, res) => {
     const postId = req.params.id;
 
-    Posts.findById(postId)
-        .then(id => {
-            if (id) {
+    if (!postId) {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    } else {
+        Posts.findById(postId)
+            .then(id => {
                 res.status(200).json(id)
-            } else {
-                res.status(404).json({ message: "The post with the specified ID does not exist." })
-            }
-        })
-        .catch(() => {
-            res.status(500).json({
-                error: "The post information could not be retrieved."
+            })
+            .catch(() => {
+                res.status(500).json({
+                    error: "The post information could not be retrieved."
+                });
             });
-        });
+    }
 });
 
 //Get request /api/posts/:id/comments
